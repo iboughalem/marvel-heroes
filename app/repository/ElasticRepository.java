8c9ea88b154bb35e5342401214d6b14d1af7b524
Ibrahim Boughalem <ibra.boughalem1995@gmail.com>
2020-02-19 16:30:00 +0100

    search & suggest heroes

--- app/repository/ElasticRepository.java
+++ app/repository/ElasticRepository.java
@@ -1,9 +1,12 @@
 package repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import env.ElasticConfiguration;
 import env.MarvelHeroesConfiguration;
 import models.PaginatedResults;
 import models.SearchedHero;
+import play.libs.Json;
 import play.libs.ws.WSClient;
 import utils.SearchedHeroSamples;
 
@@ -28,24 +31,58 @@ public class ElasticRepository {
 
 
     public CompletionStage<PaginatedResults<SearchedHero>> searchHeroes(String input, int size, int page) {
-        return CompletableFuture.completedFuture(new PaginatedResults<>(3, 1, 1, Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan())));
-        // TODO
-        // return wsClient.url(elasticConfiguration.uri + "...")
-        //         .post(Json.parse("{ ... }"))
-        //         .thenApply(response -> {
-        //             return ...
-        //         });
+        int from = (page - 1)*size;
+        return wsClient.url(elasticConfiguration.uri + "/_search")
+                .post(Json.parse("{\n" +
+                        "    \"query\": {\n" +
+                        "      \"multi_match\" : {\n" +
+                        "        \"query\":    \""+ input + "\",\n" +
+                        "        \"fields\": [ \"name^4\", \"aliases^3\", \"secret_identities^3\", \"description^2\", \"partners\" ]\n" +
+                        "      }\n" +
+                        "    },\n" +
+                        "     \"from\": " + from + "," +
+                        "      \"size\": " + size +
+                        "  }"))
+                .thenApply(response -> {
+                    JsonNode responseObj = response.asJson().get("hits");
+                    Iterator<JsonNode> hits = responseObj.withArray("hits").elements();
+                    ArrayList<SearchedHero> heroes = new ArrayList<>();
+                    while (hits.hasNext()){
+                        JsonNode hero = hits.next().get("_source");
+                        SearchedHero searchedHero = SearchedHero.fromJson(hero);
+                        heroes.add(searchedHero);
+                    }
+                    int numHeroes = responseObj.get("total").get("value").asInt();
+                    int total = (int) Math.ceil((double) numHeroes/size);
+                    return new PaginatedResults<>(numHeroes, page, total, heroes);
+                });
     }
 
-    
-
+    /*La suggestion doit tenir compte également des alias et identités secrètes des Héros*/
     public CompletionStage<List<SearchedHero>> suggest(String input) {
-        return CompletableFuture.completedFuture(Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan()));
-        // TODO
-        // return wsClient.url(elasticConfiguration.uri + "...")
-        //         .post(Json.parse("{ ... }"))
-        //         .thenApply(response -> {
-        //             return ...
-        //         });
+        return wsClient.url(elasticConfiguration.uri + "/heroes/_search")
+                .post(Json.parse(
+                        "{\n" +
+                                "    \"suggest\": {\n" +
+                                "      \"suggest-alias-secret\" : {\n" +
+                                "        \"prefix\" : \""+ input +"\",\n" +
+                                "        \"completion\" : {\n" +
+                                "          \"field\" : \"suggest\"\n" +
+                                "        }\n" +
+                                "      }\n" +
+                                "      }\n" +
+                                "  }"
+                ))
+                .thenApply(response -> {
+                    Iterator<JsonNode> hits = response.asJson()
+                            .get("suggest").withArray("suggest-alias-secret").get(0).withArray("options").iterator();
+                    ArrayList<SearchedHero> heroes = new ArrayList<>();
+                    while (hits.hasNext()){
+                        JsonNode hero = hits.next().get("_source");
+                        SearchedHero searchedHero = SearchedHero.fromJson(hero);
+                        heroes.add(searchedHero);
+                    }
+                    return heroes;
+                });
     }
 }
